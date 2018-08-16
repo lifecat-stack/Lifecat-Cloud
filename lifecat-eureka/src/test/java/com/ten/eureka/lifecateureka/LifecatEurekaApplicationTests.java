7ff4f6c4218c2851d854d31a623322e0cfd908a5
kevinten10 <596823919@qq.com>
2018-08-16 16:24:39 +0800

    vesrion2.0.1
    spring cloud project

---
+++ lifecat-eureka/src/test/java/com/ten/eureka/lifecateureka/LifecatEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ten.eureka.lifecateureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LifecatEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
